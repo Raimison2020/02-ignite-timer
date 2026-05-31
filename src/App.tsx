b10fcc6cb37e293dcd713d7fe8683b8fb4489028
Raimison Araujo <raimison.ti@gmail.com>
2026-05-31 02:01:55 -0400

    7. Cores e fontes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
@@ -11,6 +12,10 @@ export function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+
+      <h1>Preciso estudar</h1>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
